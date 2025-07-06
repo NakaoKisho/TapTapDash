a58352f512ce2bbb797ace95f5a4b3b97efeaf62
なかお <nk.110181@icloud.com>
2025-07-06 09:44:23 +0900

    changed version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vegcale.taptaptap"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
